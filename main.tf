1043fa3fd70f0620e7c7f8abe0d2fa9a382d92a3
Marcelo Mendonça <mendonca2709@gmail.com>
2021-12-19 11:05:48 -0300

    Filter out deprecated probes

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 locals {
-  probes = length(var.probe_names) > 0 ? [for k, v in data.grafana_synthetic_monitoring_probes.main.probes : v if contains(var.probe_names, k)] : values(data.grafana_synthetic_monitoring_probes.main.probes)
+  deprecated_probes = [
+    "Chicago",
+    "LosAngeles",
+    "Miami",
+    "Seattle",
+    "SanJose",
+    "Seol"
+  ]
+  available_probes = { for k, v in data.grafana_synthetic_monitoring_probes.main.probes : k => v if contains(local.deprecated_probes, k) == false }
+  probes           = length(var.probe_names) > 0 ? [for k, v in local.available_probes : v if contains(var.probe_names, k)] : values(local.available_probes)
 }
 
 data "grafana_synthetic_monitoring_probes" "main" {}
